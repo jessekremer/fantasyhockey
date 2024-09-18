f22ae3f7ecaa0ad875e93ef92f6113fb59ae361a
jessekremer <68323854+jessekremer@users.noreply.github.com>
2024-09-18 19:30:14 +0800

    simplifying model with api rewrite

--- hockeystats/models/curated/player_stats.sql
+++ hockeystats/models/curated/player_stats.sql
@@ -2,18 +2,5 @@
     config(materialized='table') 
 }}
 
-with 
-    nhl_stats_current as (select * from {{ ref('nhl_player_20232024_singleSeason') }}),
-    nhl_stats_1yago as (select * from {{ ref('nhl_player_20222023_singleSeason') }}),
-    nhl_stats_2yago as (select * from {{ ref('nhl_player_20212022_singleSeason') }}),
-    nhl_stats_3yago as (select * from {{ ref('nhl_player_20202021_singleSeason') }}),
-
-    nhl_stats as (
-        select * from nhl_stats_1yago
-        union all
-        select * from nhl_stats_2yago
-        union all
-        select * from nhl_stats_3yago
-    )
-
+with nhl_stats_current as (select * from {{ ref('nhl_games_singleSeason') }})
 select * from nhl_stats
